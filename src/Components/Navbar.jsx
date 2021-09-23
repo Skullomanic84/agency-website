9685af9a42b9c03a1aa65aa29b316a067d9761ec
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2021-09-23 18:38:41 +0200

    Updated Navbar.jsx
    populated the menu items and styled the logo and button. Added the playfair font to index.css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,20 @@ function Navbar() {
     return (
         <Container>
             <Wrap>
-                <span>
-                    logo
-                </span>
+                <Left>
+                    <Logo>
+                        Agency
+                    </Logo>
+                    <Menu>
+                        <MenuItem>Home</MenuItem>
+                        <MenuItem>Features</MenuItem>
+                        <MenuItem>Services</MenuItem>
+                        <MenuItem>Pricing</MenuItem>
+                        <MenuItem>Contact</MenuItem>
+                    </Menu>
+                </Left>
                 <Button>
-
+                    JOIN NOW
                 </Button>
             </Wrap>
         </Container>
@@ -19,10 +28,41 @@ function Navbar() {
 export default Navbar
 
 const Container = styled.div `
-    height: 60px;
-    background: #000;
+    height: 50px;
 `
 const Wrap = styled.div `
     padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `
-const Button = styled.button ``
+const Left = styled.div `
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const Logo = styled.h1 `
+    text-decoration: underline #8c7851;
+    color: #020826;
+    font-weight: 900;
+`
+const Menu = styled.ul `
+    display: flex;
+    align-items: center;
+    list-style: none;   
+`
+const MenuItem = styled.li `
+    margin-right: 30px;
+    font-size: 18px;
+    color: #020826;
+    font-weight: 600;
+`
+const Button = styled.button `
+    border: 1px solid #f9f4ef;
+    padding: 10px 15px;
+    background-color: #8c7851;
+    color: #020826;
+    font-weight: 700;
+    font-family: 'Playfair Display', serif;
+`
